9039bc1b14622629ee9441f02bbd41d2bdbb3514
farhan7479 <farhan078609@gmail.com>
2023-05-28 12:50:47 +0530

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,32 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Policy from "./pages/Policy";
+import PageNotFound from "./pages/PageNotFound";
+import Register from "./pages/auth/Register";
+import Login from "./pages/auth/Login";
+import PrivateRoute from "./components/routes/Private";
+import Dashboard from "./pages/user/Dashboard";
+import ForgotPasssword from "./pages/auth/ForgotPassword";
+function App() {
+  return (
+    <>
+       <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/dashboard" element={<PrivateRoute />}>
+          <Route path="" element={<Dashboard />} />
+        </Route>
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPasssword />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
