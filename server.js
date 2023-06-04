63e5c46062f3d072d0556c453e53b9233b27ebe8
Farhan Shahid <123633657+farhan7479@users.noreply.github.com>
2023-06-04 15:12:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
-import {fileURLToPath} from 'url';
+//import {fileURLToPath} from 'url';
 import cors from "cors";
 
 //configure env
@@ -12,8 +12,8 @@ dotenv.config();
 //databse config
 connectDB();
 
-const __filename=fileURLToPath(import.meta.url);
-const __dirname=path.dirname(__filename);
+//const __filename=fileURLToPath(import.meta.url);
+//const __dirname=path.dirname(__filename);
 
 //rest object
 const app = express();
@@ -27,7 +27,7 @@ const corsOption= {
 app.use(cors(corsOption));
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname,"./frontend/build")));
+//app.use(express.static(path.join(__dirname,"./frontend/build")));
 
 
 import authRoutes from "./routes/authRoute.js";
@@ -37,9 +37,9 @@ import path from "path";
 
 //routes
 
-app.use('*',function(req,res){
+/*app.use('*',function(req,res){
   res.sendFile(path.join(__dirname,"./frontend/build/index.html"));
-});
+});*/
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
